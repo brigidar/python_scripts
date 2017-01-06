158759646dc2bef93f55e5255d9464e1a35105ec
brigidar <brigida.a.rusconi@gmail.com>
2017-01-06 17:55:19 -0600

    partial update

--- vcf_snp_output.py
+++ vcf_snp_output.py
@@ -110,19 +110,19 @@ def missing_char(str, pos,n):
 
 #-------------------------------Parse arguments-------------------------------
 parser = argparse.ArgumentParser()
-parser.add_argument('-o', '--output', help="combined snp table and add csv extension to name")
+parser.add_argument('-o', '--output', help="combined snp table", default='output.txt')
 parser.add_argument('-g', '---genbank', help="genbank file of reference genome")
 parser.add_argument('-t', '---translation_table', help="provide translation table number", default=11)
-parser.add_argument('-i', '--input_path_ile', help="gets path to input files")
+parser.add_argument('-q', '--query', nargs='*', help="list of query files separated by spaces.", metavar="qfile.txt")
 args = parser.parse_args()
 output_file = args.output
 genbank=args.genbank
 t_t=args.translation_table
-input=args.input_path
+query=args.query
 
 
 # -------------------------------concatenates vcf files-------------------------------
-data = [read_csv(f, sep ='\t', header=None, dtype=object) for f in files]
+data = [read_csv(f, sep ='\t', header=None, dtype=object) for f in query]
 
 maxim=list()
 for d in data:

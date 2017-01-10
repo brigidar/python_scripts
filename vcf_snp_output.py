e2841fe484dc084837954188f923cc48f9812629
brigidar <brigida.a.rusconi@gmail.com>
2017-01-09 18:03:17 -0600

    update
    matches nucleotide to strand direction

--- vcf_snp_output.py
+++ vcf_snp_output.py
@@ -2,18 +2,18 @@
 #########################################################################################
 #											#
 # Name	      :	vcf_snp_output.py								#
-# Version     : 0.1								#
+# Version     : 0.3								#
 # Project     : snp verify reads						#
 # Description : Script to populate SNPs identified from reads with location information		#
 # Author      : Brigida Rusconi								#
-# Date        : September 6th, 2016							#
+# Date        : January 9th, 2017						#
 #											#
 #########################################################################################
 
 
 import argparse, os, sys, csv,shutil,tempfile,operator
 import pdb
-import glob
+import glob,logging
 from pandas import *
 from numpy import *
 from glob import glob
@@ -27,7 +27,7 @@ from Bio.SeqFeature import FeatureLocation
 # There is only one Genbank file read in as there is only one reference.
 #-----------------------------------------------------------------------
 
-# Description   : Parse reference GenBank filesto obtain the gene information for reference genome
+# Description   : Parse reference GenBank files to obtain the gene information for reference genome
 # Parameters    : gbk_files = Path to the list of files of reference GenBank genomes
 # Returns       : retval = Reference to a hash of gene information. Key = Molecule name, gene name, tags like start, end, length, strand, gene, pseudo, product
 #	Value = Annotation of each gene
@@ -103,23 +103,38 @@ def get_trans_state(base, hit):
         return "Error non canonical base"
 #--------------------------------------------------------------------------------------------
 def missing_char(str, pos,n):
-    str=str.replace(str[pos],n)
+    item=list(str)
+    item[pos]=n
+    str="".join(item)
     return str
-#--------------------------------End Functions------------------------------------------------------------
+#--------------------------------------------------------------------------------------------
+# to invert nucleotide if gene is on opposite strand
+def invert_nucl(nuc):
+    nucleo=["A","G","T","C","N","R","Y", "S","W","K","M","B","V","D","H"]
+    n_inv=["T","C","A","G","N","Y","R","S","W","M","K","V","B","H","D"]
+    comb=zip(nucleo,n_inv)
+    nucl_dict={}
+    for nucleo,n_inv in comb:
+        nucl_dict[nucleo]=n_inv
+    nuc2=nucl_dict[nuc]
+    return nuc2
 
+#--------------------------------End Functions------------------------------------------------------------
+options.mode.chained_assignment = None
 
 #-------------------------------Parse arguments-------------------------------
 parser = argparse.ArgumentParser()
 parser.add_argument('-o', '--output', help="combined snp table", default='output.txt')
 parser.add_argument('-g', '---genbank', help="genbank file of reference genome")
 parser.add_argument('-t', '---translation_table', help="provide translation table number", default=11)
-parser.add_argument('-q', '--query', nargs='*', help="list of query files separated by spaces.", metavar="qfile.txt")
+parser.add_argument('-q', '--query', nargs='*', help="list of query files separated by spaces", metavar="qfile.dat")
 args = parser.parse_args()
 output_file = args.output
 genbank=args.genbank
-t_t=args.translation_table
+t_t=int(args.translation_table)
 query=args.query
-
+#LOG_FILENAME='read_snp.log'
+#logging.basicConfig(filename=LOG_FILENAME,level=logging.WARNING)
 
 # -------------------------------concatenates vcf files-------------------------------
 data = [read_csv(f, sep ='\t', header=None, dtype=object) for f in query]
@@ -136,10 +151,11 @@ for i,x in enumerate(df[4]):
     else:
         pass
 files1=[]
-for f in files:
-    files1.append(f.split('.txt')[0])
+for f in query:
+    files1.append(f.split('.')[0])
 
 df.rename(columns={0:'molecule',1:'refpos',3:'refbase',4:('qbase:'+files1[pos])},inplace=True)
+#pdb.set_trace()
 df.drop_duplicates(inplace=True)
 df.set_index(['molecule','refpos'], inplace=True)
 
@@ -165,14 +181,15 @@ for i,f in enumerate(df['molecule']):
     if '|' in f:
         df['molecule'][i]=f.split('|')[-1]
 
+
 #---------------------read in genbank file features-----------------------------------------------
 gblist=parse_genbank_file_list(genbank)
 
 
 #---------------make dataframe with additional columns to fill out-------------------------
 filler=repeat('intergenic',df.index.size).tolist()
-sec= DataFrame({'gene_name':filler,'gene_start':filler,'gene_end':filler,'gene_length':filler,'snps_per_gene':filler,'pos_in_gene':filler,'ref_codon':filler,'ref_aa':filler,'query_codon':filler,'query_aa':filler,'transition/transversion':filler,'snps/gene_length':filler,'dn/ds':filler,'product':filler})
-sec=sec.reindex_axis(['gene_name','gene_start','gene_end','gene_length','snps_per_gene','pos_in_gene','ref_codon','ref_aa','query_codon','query_aa','transition/transversion','snps/gene_length','dn/ds','product'],axis=1)
+sec= DataFrame({'gene_name':filler,'gene_start':filler,'gene_end':filler,'gene_length':filler,'snps_per_gene':filler,'pos_in_gene':filler,'ref_codon':filler,'ref_aa':filler,'query_codon':filler,'query_aa':filler,'transition/transversion':filler,'snps/gene_length':filler,'dn/ds':filler,'product':filler,'strand':filler})
+sec=sec.reindex_axis(['gene_name','gene_start','gene_end','gene_length','snps_per_gene','pos_in_gene','ref_codon','ref_aa','query_codon','query_aa','transition/transversion','snps/gene_length','dn/ds','product','strand'],axis=1)
 
 df2=concat([df,sec],axis=1)
 
@@ -200,6 +217,7 @@ for v,f in enumerate(df['molecule']):
 #------------------------------- gene_length, name, start,stop, pos_in_gene, product--------------------------
 for i,item in enumerate(df2.index):
     for v,gh in enumerate(gene_hits):
+        
         if df2['molecule'][i]==gh[1] and df2['refpos'][i]==gh[2]:
             if gh[0].type=='gene':
                 df2['gene_length'][i]=len((gh[0].location))
@@ -209,10 +227,12 @@ for i,item in enumerate(df2.index):
                     df2['gene_start'][i]=gh[0].location.start.position
                     df2['gene_end'][i]=gh[0].location.end.position
                     df2['pos_in_gene'][i]=int(gh[2])-(gh[0].location.start.position+1)
+                    df2['strand'][i]=1
                 else:
                     df2['gene_start'][i]=gh[0].location.end.position
                     df2['gene_end'][i]=gh[0].location.start.position
-                    df2['pos_in_gene'][i]=gh[0].location.end.position-int(gh[2])+1
+                    df2['pos_in_gene'][i]=gh[0].location.end.position-int(gh[2])
+                    df2['strand'][i]=-1
             else:
                 df2['product'][i]=''.join(gh[0].qualifiers['product'])
 
@@ -235,6 +255,7 @@ for i,item in enumerate(df2['pos_in_gene']):
             pos1.append(item)
 
 # get query base information
+
 df2.rename(columns={2:'syn?'}, inplace=True)
 count_qbase=list(df2.columns.values)
 qindexes=[]
@@ -248,26 +269,38 @@ df3=df2.iloc[:,qindexes]
 snp_nb=list()
 for i,item in enumerate(df3.index):
     snp_u=[n for n in unique(df3.iloc[i,:])[:] if n!=df2.refbase[i]]
-    snp_nb.append(snp_u)
+    if len(snp_u)>0:
+        snp_nb.append(snp_u)
+    else:
+        snp_nb.append([n for n in unique(df3.iloc[i,:])[:]])
+
+for i,item in enumerate(snp_nb):
+    if len(item)<1:
+        snp_nb[i]=[n for n in unique(df3.iloc[1,:])[:]]
+for i,item in enumerate(snp_nb):
+    if snp_nb[i][0]==df2.refbase[i]:
+        df2['syn?'][i]='No SNP'
 
 
 #-------------------------------transition/transversion -------------------------------
 for g,item in enumerate(df2.index):
     tt=list()
-    for i in snp_nb:
-        if len(snp_nb)==1:
-            tt.append(get_trans_state(df2['refbase'][g],i))
+    if df2['syn?'][g]=='No SNP' and df2['gene_name'][g]!='intergenic':
+        tt.append('No SNP')
+    elif df2['gene_name'][g]=='intergenic':
+        tt.append('intergenic')
+    elif len(snp_nb[g])==1 and df2['gene_name'][g]!='intergenic' and df2['syn?'][g]!='No SNP':
+        tt.append(get_trans_state(df2['refbase'][g],snp_nb[g][0]))
     else:
-        for n in i:
-            if i!='No Hit':
+        for n in snp_nb[g]:
+            if n!='No Hit':
                 tt.append(get_trans_state(df2['refbase'][g],n))
-    df2['transition/transversion']=str('/'.join(tt))
-
+    df2['transition/transversion'][g]=str('/'.join(tt))
 
 
 #-------------------------------get snps per gene-------------------------------
-
-snps_gene=df2.groupby('gene_name').size().reset_index()
+#only genes with SNPs and not identical locations
+snps_gene=df2[df2['syn?']!='No SNP'].groupby('gene_name').size().reset_index()
 snps_gene2=list()
 for i,v in enumerate(snps_gene.index):
     col=[n for n in snps_gene.iloc[i,:][:]]
@@ -276,17 +309,23 @@ for i,v in enumerate(snps_gene.index):
 
 for i,v in enumerate(snps_gene2):
     for l,s in enumerate(df2['gene_name']):
-        if s=='intergenic':
+        if v[0]==s and s!='intergenic':
+            df2['snps_per_gene'][l]=v[1]
+        elif s=='intergenic' and df2['syn?'][l]!='No SNP':
             df2['snps_per_gene'][l]='intergenic'
-        elif s ==v[0]:
-            df2['snps_per_gene'][l]=snps_gene2[i][1]
+        elif df2['syn?'][l]=='No SNP'and s!='intergenic':
+            df2['snps_per_gene'][l]=0
+
+
+
 
 
 #-------------------------------snps per gene length-------------------------------
 for x,i in enumerate(df2.snps_per_gene):
-    if i != 'intergenic':
+    if i==0:
+        df2['snps/gene_length'][x]=0
+    elif i != 'intergenic':
         df2['snps/gene_length'][x]=float(i)/float(df2.gene_length[x])
-
 
 
 # -------------------------------query codon & aa-------------------------------
@@ -294,40 +333,58 @@ for x,i in enumerate(df2.snps_per_gene):
 
 for i,item in enumerate(df2.ref_codon):
     if item != 'intergenic':
-        if len(snp_nb[i])==1:
+        if len(snp_nb[i])==1 and df2['strand'][i]==1:
             item1=missing_char(item,pos1[i],snp_nb[i][0])
+            df2['query_codon'][i]=item1
+            df2['query_aa'][i]=str(Seq(df2['query_codon'][i], generic_dna).translate(table=t_t))
+        elif len(snp_nb[i])==1 and df2['strand'][i]!=1:
+            g=invert_nucl(snp_nb[i][0])
+            item1=missing_char(item,pos1[i],g)
             df2['query_codon'][i]=item1
             df2['query_aa'][i]=str(Seq(df2['query_codon'][i], generic_dna).translate(table=t_t))
         else:
             mult=list()
             mult_aa=list()
-            for n in snp_nb[i] and n!='No Hit':
-                item1=missing_char(item,pos1[i],n)
-                aa1=str(Seq(item1, generic_dna).translate(table=t_t))
-                mult.append(item1)
-                mult_aa.append(aa1)
+            
+            for n in snp_nb[i]:
+                if df2['strand'][i]==1 and n!='No Hit':
+                    item1=missing_char(item,pos1[i],n)
+                    aa1=str(Seq(item1, generic_dna).translate(table=t_t))
+                    mult.append(item1)
+                    mult_aa.append(aa1)
+                elif n!='No Hit':
+                    g=invert_nucl(n)
+                    item1=missing_char(item,pos1[i],g)
+                    aa1=str(Seq(item1, generic_dna).translate(table=t_t))
+                    mult.append(item1)
+                    mult_aa.append(aa1)
                 df2['query_codon'][i]=str('/'.join(mult))
                 df2['query_aa'][i]=str('/'.join(mult_aa))
 
+for i,item in enumerate(df2.ref_codon):
+    if df2['syn?'][i]=='No SNP':
+        df2['query_codon'][i]=item
+        df2['query_aa'][i]=df2.ref_aa[i]
 
 # -------------------------------synonymous nonsynonymous-------------------------------
 for i,item in enumerate(df2.query_aa):
     if '/' in item:
+        mult=list()
         for n in item.split('/'):
-            mult=list()
-            if item==df2.ref_aa[i]:
+            if n==df2.ref_aa[i]:
                 mult.append('SYN')
             else:
                 mult.append('NSYN')
-            df2['syn?'][i]='/'.join(mult)
+        df2['syn?'][i]='/'.join(mult)
     else:
         if item=='intergenic':
             df2['syn?'][i]='intergenic'
-        elif item==df2.ref_aa[i]:
+        elif item==df2.ref_aa[i] and  df2['syn?'][i]!='No SNP':
             df2['syn?'][i]='SYN'
-        else:
+        elif df2['syn?'][i]!='No SNP':
             df2['syn?'][i]='NSYN'
 
+#pdb.set_trace()
 
 #-------------------------------dn/ds-------------------------------
 dn=df2.groupby('gene_name')['syn?']
@@ -354,7 +411,6 @@ for name,group in dn:
 
 flat_dn=[n for item in counter for n in item]
 df2['dn/ds'].update(Series(flat_dn))
-
 
 #-------------------------------write file-------------------------------
 df2.sort(columns=['molecule','refpos'],inplace=True)
